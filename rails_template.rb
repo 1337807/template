f9569ece6b1cc78eb43cf593b75a47ba7869ebfe
Jonan Scheffler <jonanscheffler@gmail.com>
2014-05-24 00:09:02 -0700

    Add bootstrap, append_to_file method and features directory.
    Add bootstrap, append_to_file method and features directory.

--- rails_template.rb
+++ rails_template.rb
@@ -8,6 +8,11 @@ def prepend_file(filename, line)
   File.write(filename, "#{line}\n" + contents)
 end
 
+def append_to_file(filename, line)
+  contents = File.read(filename)
+  File.write(filename, contents + "\n#{line}\n")
+end
+
 run "psql -U jonan --command='set role admin; create role #{@app_name} with createdb; alter role #{@app_name} with login'"
 
 ['test', 'development', 'production'].each do |environment|
@@ -16,6 +21,7 @@ end
 
 gem "devise"
 gem "cancan"
+gem "bootstrap-sass"
 
 gem_group :development, :test do
   gem "capybara"
@@ -34,5 +40,9 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 capybara
+run "mkdir spec/features"
+
+append_to_file('app/assets/stylesheets/application.css', '@import "bootstrap";')
+run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss"
 
 migrate
